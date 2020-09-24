f48db5cfa718c8df03564240ce49f06b3e8a77fa
Wisscas <is.jaras@gmail.com>
2020-09-23 23:31:18 -0300

    init update

--- neurosystems/__init__.py
+++
@@ -1,5 +0,0 @@
-cuSNN is a C++ library that enables GPU-accelerated simulations of large-scale Spiking Neural Networks (SNNs). -> https://github.com/tudelft/cuSNN
-
-Spiking Neural Networks for More Efficient AI Algorithms -> https://www.youtube.com/watch?v=PeW-TN3P1hk
-
-Networks of Spiking Neurons Learn to Learn and Remember -> https://www.youtube.com/watch?v=h2qqKJcULng
