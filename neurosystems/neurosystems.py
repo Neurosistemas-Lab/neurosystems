fd6e3e2bf88f86d57bb32d1a1d05dd3aecc1f569
Wisscas <is.jaras@gmail.com>
2020-09-23 22:18:23 -0300

    initial code for test 0

---
+++ neurosystems/neurosystems.py
@@ -0,0 +1,10 @@
+def tets(my_name):
+    """
+    Print a line for testing package import
+    Args:
+        my_name (str): person's name
+    Returns:
+        None
+    """
+
+    print(f"I'll contain all Neurosystema's Lab functions some day, {my_name}.")
