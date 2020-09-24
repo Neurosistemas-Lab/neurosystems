0c63e67624f91f0bc803b217db0d64e7468c1626
Wisscas <is.jaras@gmail.com>
2020-09-23 22:27:35 -0300

    setup.py creation for v0.0.1

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as readme_file:
+    readme = readme_file.read()
+
+requirements = ["ipython>=6", "nbformat>=4", "nbconvert>=5", "requests>=2"]
+
+setup(
+    name="neurosystems",
+    version="0.0.1",
+    author="Ismael Jaras",
+    author_email="is.jaras@gmail.com",
+    description="A package containing all Neurosistemas Lab's codes",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Neurosistemas-Lab/neurosystems",
+    packages=find_packages(),
+    install_requires=requirements,
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+    ],
+)
